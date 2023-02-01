99e3f69b179cef47aa76a8eaba8bf9e40c88f1be
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-02-01 19:15:05 +0530

    new changes

--- src/views/pages/account/EditProfile.jsx
+++ src/views/pages/account/EditProfile.jsx
@@ -16,6 +16,7 @@ import { Register, UpdateProfile } from "../../../graphql/mutations";
 import { SignIn, UserDetails } from "../../../graphql/query";
 import { useWeb3React } from "@web3-react/core";
 import { Link, useHistory } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 import {
   ACCOUNT_CREATE_ALERT,
   ACCOUNT_UPDATE_ALERT,
@@ -28,7 +29,21 @@ const EditProfile = () => {
   const history = useHistory();
   const [updateProfile] = useMutation(UpdateProfile);
   const [register] = useMutation(Register);
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [userProfileData, setUserProfileData] = useState({
+    username: "",
+    firstname: "",
+    lastname: "",
+    bgImage: "",
+    avatarUrl: "",
+    coverUrl: "",
+    aboutDetails: "",
+    facebookUrl: "",
+    websiteUrl: "",
+    twitterUrl: "",
+  });
+
+  const { uploadOnIpfs } = useStorage();
   const { data: userData } = useQuery(UserDetails, {
     skip: !active,
     variables: {
@@ -40,95 +55,112 @@ const EditProfile = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  const handleChange = (e) => {
+    setUserProfileData({ ...userProfileData, [e.target.name]: e.target.value });
+    console.log(userProfileData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    let avatarImage = await uploadOnIpfs(userProfileData.avatarUrl);
+    let coverImage = await uploadOnIpfs(userProfileData.coverUrl);
+
+    let newavatarImage = avatarImage.replace(
+      "ipfs://",
+      "https://gateway.ipfscdn.io/ipfs/"
+    );
+    let newConverImage = coverImage.replace(
+      "ipfs://",
+      "https://gateway.ipfscdn.io/ipfs/"
+    );
+    console.log(newavatarImage);
+    console.log(newConverImage);
+
+    const User = z.object({
+      username: z.string().max(20).trim(),
+      firstname: z.string().max(20).trim(),
+      lastname: z.string().max(20).trim(),
+      aboutDetails: z.string().max(200).trim(),
+      facebookUrl: z.string().url().trim(),
+      websiteUrl: z.string().url().trim(),
+      twitterUrl: z.string().url().trim(),
+    });
+
+    let data = User.safeParse({
+      username: document.getElementById("username").value,
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      bgImage: newConverImage,
+      avatarUrl: newavatarImage,
+      aboutDetails: document.getElementById("about").value,
+      facebookUrl: document.getElementById("facebookUrl").value,
+      websiteUrl: document.getElementById("websiteUrl").value,
+      twitterUrl: document.getElementById("twitterUrl").value,
+    });
+
+    if (data.success) {
+      if (userData?.user) {
+        updateProfile({
+          variables: { userId: userData?.user?._id, ...data.data },
+          refetchQueries: [
+            {
+              query: UserDetails,
+              variables: {
+                walletAddress: account,
+              },
+            },
+            {
+              query: SignIn,
+              variables: {
+                walletAddress: account,
+              },
+            },
+          ],
+        }).then((res) => {
+          // console.log(res);
+          ACCOUNT_UPDATE_ALERT().then(() => history.push("/user-profile"));
+        });
+      } else {
+        register({
+          variables: { walletAddress: account, ...data.data },
+          refetchQueries: [
+            {
+              query: UserDetails,
+              variables: {
+                walletAddress: account,
+              },
+            },
+            {
+              query: SignIn,
+              variables: {
+                walletAddress: account,
+              },
+            },
+          ],
+        })
+          .then((res) => {
+            console.log(res);
+            ACCOUNT_CREATE_ALERT().then(() => history.push("/user-profile"));
+          })
+          .catch((res) => console.log(res));
+      }
+    } else {
+      Swal.fire(
+        "Fill Correct information",
+        "Please fill all the required columns"
+      );
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div className="edit_profile">
       <Header />
       <HeroEditProfile />
       <div className="container">
         <div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const User = z.object({
-                username: z.string().max(20).trim(),
-                firstname: z.string().max(20).trim(),
-                lastname: z.string().max(20).trim(),
-                bgImage: z.string().url().trim(),
-                avatarUrl: z.string().url().trim(),
-                aboutDetails: z.string().max(200).trim(),
-                facebookUrl: z.string().url().trim(),
-                websiteUrl: z.string().url().trim(),
-                twitterUrl: z.string().url().trim(),
-              });
-
-              let data = User.safeParse({
-                username: document.getElementById("username").value,
-                firstname: document.getElementById("firstname").value,
-                lastname: document.getElementById("lastname").value,
-                bgImage: document.getElementById("coverUrl").value,
-                avatarUrl: document.getElementById("avatarUrl").value,
-                aboutDetails: document.getElementById("about").value,
-                facebookUrl: document.getElementById("facebookUrl").value,
-                websiteUrl: document.getElementById("websiteUrl").value,
-                twitterUrl: document.getElementById("twitterUrl").value,
-              });
-
-              if (data.success) {
-                if (userData?.user) {
-                  updateProfile({
-                    variables: { userId: userData?.user?._id, ...data.data },
-                    refetchQueries: [
-                      {
-                        query: UserDetails,
-                        variables: {
-                          walletAddress: account,
-                        },
-                      },
-                      {
-                        query: SignIn,
-                        variables: {
-                          walletAddress: account,
-                        },
-                      },
-                    ],
-                  }).then((res) => {
-                    // console.log(res);
-                    ACCOUNT_UPDATE_ALERT().then(() =>
-                      history.push("/user-profile")
-                    );
-                  });
-                } else {
-                  register({
-                    variables: { walletAddress: account, ...data.data },
-                    refetchQueries: [
-                      {
-                        query: UserDetails,
-                        variables: {
-                          walletAddress: account,
-                        },
-                      },
-                      {
-                        query: SignIn,
-                        variables: {
-                          walletAddress: account,
-                        },
-                      },
-                    ],
-                  })
-                    .then((res) => {
-                      console.log(res);
-                      ACCOUNT_CREATE_ALERT().then(() =>
-                        history.push("/user-profile")
-                      );
-                    })
-                    .catch((res) => console.log(res));
-                }
-              } else {
-                // console.log(data.error.issues);
-                Swal.fire("Fill Correct information", "Please fill all the required columns");
-              }
-            }}
-          >
+          <form onSubmit={handleSubmit}>
             <div
               className="box edit_box col-lg-9 space-y-30"
               style={{ margin: "0 auto " }}
@@ -142,7 +174,9 @@ const EditProfile = () => {
                         <span className="nameInput">Username</span>
                         <input
                           id="username"
+                          name="username"
                           type="text"
+                          onChange={handleChange}
                           defaultValue={userData?.user?.username}
                           placeholder="Username"
                           maxLength="20"
@@ -153,7 +187,9 @@ const EditProfile = () => {
                         <span className="nameInput">First Name</span>
                         <input
                           id="firstname"
+                          name="firstname"
                           type="text"
+                          onChange={handleChange}
                           defaultValue={userData?.user?.firstname}
                           className="form-control"
                           placeholder="First Name"
@@ -164,15 +200,29 @@ const EditProfile = () => {
 
                       <div className="space-y-10">
                         <span className="nameInput">Cover URL</span>
-                        <input
+                        {/* <input
                           type="text"
                           id="coverUrl"
                           defaultValue={userData?.user?.bg_image}
                           className="form-control"
                           placeholder="Cover URL"
                           required
+                        /> */}
+                        <input
+                          type="file"
+                          id="coverUrl"
+                          name="coverUrl"
+                          onChange={(e) => {
+                            setUserProfileData({
+                              ...userProfileData,
+                              coverUrl: e.target.files[0],
+                            });
+                          }}
+                          className="form-control"
+                          placeholder="Cover URL"
+                          required
                         />{" "}
-                        <ImageModal text={"Cover Uplaod"} />
+                        {/* <ImageModal text={"Cover Uplaod"} /> */}
                       </div>
                     </div>
                   </div>
@@ -185,21 +235,39 @@ const EditProfile = () => {
                     <div className="space-y-40">
                       <div className="space-y-10">
                         <span className="nameInput">Avatar URL</span>
-                        <input
+                        {/* <input
                           type="text"
                           id="avatarUrl"
+                          name="avatarUrl"
                           defaultValue={userData?.user?.avatar_url}
                           className="form-control"
                           placeholder="Avatar URL"
                           required
+                        /> */}
+                        <label htmlFor="avatarUrl"></label>
+                        <input
+                          type="file"
+                          onChange={(e) =>
+                            setUserProfileData({
+                              ...userProfileData,
+                              avatarUrl: e.target.files[0],
+                            })
+                          }
+                          id="avatarUrl"
+                          name="avatarUrl"
+                          className="form-control"
+                          placeholder="Avatar URL"
+                          required
                         />
-                        <ImageModal text={"Avatar Uplaod"} />
+                        {/* <ImageModal text={"Avatar Uplaod"} /> */}
                       </div>
                       <div className="space-y-10">
                         <span className="nameInput">Last Name</span>
                         <input
                           id="lastname"
+                          name="lastname"
                           type="text"
+                          onChange={handleChange}
                           defaultValue={userData?.user?.lastname}
                           className="form-control"
                           placeholder="Last Name"
@@ -213,11 +281,12 @@ const EditProfile = () => {
               </div>
               <div className="space-y-10">
                 <span className="nameInput">About</span>
-
                 <textarea
                   style={{ minHeight: 110 }}
                   placeholder="Add your bio"
+                  name="aboutDetails"
                   id="about"
+                  onChange={handleChange}
                   defaultValue={userData?.user?.about_details}
                   maxLength="150"
                   required
@@ -234,6 +303,8 @@ const EditProfile = () => {
                           type="text"
                           className="form-control"
                           id="facebookUrl"
+                          onChange={handleChange}
+                          name="facebookUrl"
                           defaultValue={userData?.user?.facebookUrl}
                           placeholder="facebook URL"
                         />
@@ -243,6 +314,8 @@ const EditProfile = () => {
                         <input
                           type="text"
                           id="websiteUrl"
+                          name="websiteUrl"
+                          onChange={handleChange}
                           defaultValue={userData?.user?.websiteUrl}
                           className="form-control"
                           placeholder="Website URL"
@@ -262,6 +335,8 @@ const EditProfile = () => {
                         <input
                           type="text"
                           id="twitterUrl"
+                          onChange={handleChange}
+                          name="twitterUrl"
                           defaultValue={userData?.user?.twitterUrl}
                           className="form-control"
                           placeholder="Twitter URL"
@@ -279,127 +354,26 @@ const EditProfile = () => {
                 changes.
               </p>
               <div>
-                <button type="submit" className="btn btn-grad btn-border">
-                  {userData?.user ? "Update" : "Create"} Profile
-                </button>
+                {isLoading ? (
+                  <Spinner />
+                ) : (
+                  <button
+                    disabled={isLoading}
+                    type="submit"
+                    className="btn btn-grad btn-border"
+                  >
+                    {userData?.user ? "Update" : "Create"}
+                    Profile
+                  </button>
+                )}
               </div>
             </div>
           </form>
         </div>
       </div>
-
       <Footer />
     </div>
   );
 };
-const ImageModal = ({ text }) => {
-  const { getImageLink } = useStorage();
-  const [previewURL, setPreviewUrl] = useState(null);
-  const [copyLink, setCopyLink] = useState(null);
-  const [upload, setUpload] = useState(false);
-  const [open, setOpen] = useState(false);
-  useEffect(() => {
-    setCopyLink(null);
-  }, []);
-  return (
-    <>
-      <>
-        <p
-          style={{
-            fontSize: "12px",
-            color: "#ff4d4f",
-            cursor: "pointer",
-          }}
-          onClick={() => setOpen(true)}
-          data-toggle="modal"
-          data-target="#exampleModal"
-        >
-          {text}
-        </p>
 
-        <Modal
-          footer={null}
-          title={text}
-          centered
-          open={open}
-          onOk={() => setOpen(false)}
-          onCancel={() => setOpen(false)}
-        >
-          <Upload.Dragger
-            name="logo"
-            listType="picture"
-            onChange={(e) => {
-              // console.log(e);
-              setPreviewUrl(e.fileList[0].originFileObj);
-            }}
-          >
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibit from
-              uploading company data or other band files
-            </p>
-          </Upload.Dragger>
-
-          {copyLink !== null && (
-            <p className="mt-3">
-              Copy your {text.toLowerCase()}
-              {" : "}
-              <Link>
-                <span
-                  onClick={() => {
-                    navigator.clipboard.writeText(copyLink);
-                    alert("Link copied");
-                    setOpen(false);
-                  }}
-                >
-                  Copy Link
-                </span>
-              </Link>
-            </p>
-          )}
-
-          <button
-            class="mt-5 w-full ant-btn ant-btn-primary btn btn-grad"
-            type="button"
-            disabled={upload}
-          >
-            {upload ? (
-              <>
-                <span
-                  class="spinner-border spinner-border-sm"
-                  role="status"
-                  aria-hidden="true"
-                >
-                  {" "}
-                </span>{" "}
-                <span> Uploading...</span>
-              </>
-            ) : (
-              <>
-                {" "}
-                <div
-                  className="w-full"
-                  onClick={async () => {
-                    setUpload(true);
-                    let image = await getImageLink({ image: previewURL });
-                    setCopyLink(image.image);
-                    setUpload(false);
-                  }}
-                >
-                  {" "}
-                  Upload{" "}
-                </div>
-              </>
-            )}
-          </button>
-        </Modal>
-      </>
-    </>
-  );
-};
 export default EditProfile;
