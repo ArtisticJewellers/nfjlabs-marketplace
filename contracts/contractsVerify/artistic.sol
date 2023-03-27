965ed4d03a5c9fccf0ae6ed7b252042d62e9e9d6
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-03-27 20:11:31 +0530

    fixing imports

--- contracts/contractsVerify/artistic.sol
+++ contracts/contractsVerify/artistic.sol
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
 contract ArtisticJeweller is
     Initializable,
     ERC721Upgradeable,
