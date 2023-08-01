a8c553700e5e8daf0e05a9bdc05b97f2a36d46aa
AnandJNairGit <anandjnairit@gmail.com>
2023-08-01 11:41:13 +0530

    change the ipfs gateway

--- src/config/constant/contract.js
+++ src/config/constant/contract.js
@@ -1,5 +1,6 @@
 export const NFT_CONTRACT = "0xec74932C53c81b7f174eaa0D881Db50CE05a38f2";
 export const NFT_MARKETPLACE_CONTRACT =
   "0xB7C470715697D20FE56c6a651294E3aaC67FF9d8";
-export const IPFS_URL = "https://gateway.ipfscdn.io/ipfs/";
+// export const IPFS_URL = "https://gateway.ipfscdn.io/ipfs/";
+export const IPFS_URL = "https://ipfs.moralis.io/ipfs/";
 export const MIN_PRICE = 0.0000001;
